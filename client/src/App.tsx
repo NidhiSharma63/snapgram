025d82d0461398a7573acdf6eefa35eef141e648
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-11-14 13:18:52 +0530

    dark mode added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,27 @@
 import "@/index.css";
+import { useEffect, useState } from "react";
+
+const darkModeValueInLocalStorage = localStorage.getItem("theme-for-snapgram");
+
 function App() {
+  const [darkModeEnabled, setDarkModeEnabled] = useState(
+    darkModeValueInLocalStorage ? JSON.parse(darkModeValueInLocalStorage) : false
+  );
+
+  useEffect(() => {
+    if (darkModeEnabled) {
+      window.document.documentElement.classList.add("dark");
+      localStorage.setItem("theme-for-snapgram", "true");
+    } else {
+      window.document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme-for-snapgram", "false");
+    }
+  }, [darkModeEnabled]);
+
   return (
     <>
-      <h1 className="text-2xl font-light underline">Hello world!</h1>
+      <p onClick={() => setDarkModeEnabled((prev: boolean) => !prev)}>dark mode add</p>
+      <h1 className="text-2xl font-light text-cyan-800 underline">Hello world!</h1>
     </>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: [
     "./index.html", // <= add this
     "./src/**/*.{js,ts,jsx,tsx}", // <= no spaces
