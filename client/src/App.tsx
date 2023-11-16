660e34a54b6652e79301f00f096b278e1dc43aa2
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-11-16 07:20:43 +0530

    shadcn added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import "@/index.css";
 import { useEffect, useState } from "react";
 
 const darkModeValueInLocalStorage = localStorage.getItem("theme-for-snapgram");
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,5 @@
 import App from "@/App";
-import "@/index.css";
+import "@/css/index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
