813064174894cc3c6eac4d8481f538d286ac6d3b
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-11-16 07:47:49 +0530

    dark theme added using shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { useTheme } from "@/context/themeProviders";
 import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
 const darkModeValueInLocalStorage = localStorage.getItem("theme-for-snapgram");
 
@@ -6,6 +9,7 @@ function App() {
   const [darkModeEnabled, setDarkModeEnabled] = useState(
     darkModeValueInLocalStorage ? JSON.parse(darkModeValueInLocalStorage) : false
   );
+  const { setTheme } = useTheme();
 
   useEffect(() => {
     if (darkModeEnabled) {
@@ -18,10 +22,27 @@ function App() {
   }, [darkModeEnabled]);
 
   return (
-    <>
-      <p onClick={() => setDarkModeEnabled((prev: boolean) => !prev)}>dark mode add</p>
-      <h1 className="text-2xl font-light text-cyan-800 underline">Hello world!</h1>
-    </>
+    <Routes>
+      {/* <Route element={<AuthLayout />}>
+        <Route path="/sign-up" element={<SignUpForm />} />
+        <Route path="/sign-in" element={<SignInForm />} />
+      </Route> */}
+      <Route
+        path="/"
+        element={
+          <>
+            <p onClick={() => setDarkModeEnabled((prev: boolean) => !prev)}>dark mode add</p>
+            <h1 className="text-2xl font-light text-cyan-800 underline">Hello world!</h1>
+            <Button variant="outline" onClick={() => setTheme("dark")}>
+              Change the theme plz to Dark
+            </Button>
+            <Button variant="outline" onClick={() => setTheme("light")}>
+              Change the theme plz to light
+            </Button>
+          </>
+        }
+      />
+    </Routes>
   );
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
 import App from "@/App";
+import { ThemeProvider } from "@/context/themeProviders";
 import "@/css/index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="light" storageKey="snap-gram-theme">
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
