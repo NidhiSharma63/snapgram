836ed2367a0f0cc0d3ce74962fb561a65be6e29c
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-11-18 12:11:20 +0530

    edit icons displayed

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -3,8 +3,9 @@
 // import { useAuthContext } from "@/context/AuthContext";
 // import { useGetUserById } from "@/lib/react-query/queryAndMutations";
 import Loader from "@/components/shared/Loader";
+import { useUserDetail } from "@/context/userContext";
 import useAuth from "@/hooks/query/useAuth";
-import { Outlet, useLocation, useParams } from "react-router-dom";
+import { Link, Outlet, useLocation, useParams } from "react-router-dom";
 // import LikedPosts from "./LikedPosts";
 
 interface StabBlockProps {
@@ -23,6 +24,7 @@ const Profile = () => {
   const { id } = useParams();
   const { useGetUserById } = useAuth();
   const { data } = useGetUserById(id || "");
+  const { userDetails } = useUserDetail();
   console.log(data);
   //   const { user } = useAuthContext();
   const { pathname } = useLocation();
@@ -61,16 +63,16 @@ const Profile = () => {
           </div>
 
           <div className="flex justify-center gap-4">
-            {/* <div className={`${user.id !== currentUser.$id && "hidden"}`}> */}
-            {/* <Link
-                to={`/update-profile/${currentUser.$id}`}
-                className={`h-12 bg-dark-4 px-5 text-light-1 flex-center gap-2 rounded-lg ${
-                  user.id !== currentUser.$id && "hidden"
+            <div className={`${userDetails && userDetails._id !== data._id && "hidden"}`}>
+              <Link
+                to={`/update-profile/${data._id}`}
+                className={`h-12 dark:bg-dark-4 px-5 bg-off-white text-light-1 flex-center gap-2 rounded-lg ${
+                  userDetails && userDetails._id !== data._id && "hidden"
                 }`}>
                 <img src={"/assets/icons/edit.svg"} alt="edit" width={20} height={20} />
-                <p className="flex whitespace-nowrap small-medium">Edit Profile</p>
-              </Link> */}
-            {/* </div> */}
+                <p className="flex whitespace-nowrap small-medium dark:text-white text-black">Edit Profile</p>
+              </Link>
+            </div>
             {/* <div className={`${user.id === id && "hidden"}`}>
               <Button type="button" className="shad-button_primary px-8">
                 Follow
