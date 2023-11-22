0b6ded8c810db802b6e44c40972ef4e7c2fdc8d6
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-11-22 12:41:21 +0530

    like color changed

--- client/src/components/shared/PostStats.tsx
+++ client/src/components/shared/PostStats.tsx
@@ -75,7 +75,7 @@ function PostStats({ postId, likes }: { postId: string; likes: string[] }) {
           />
         )}
 
-        <p className="small-medium lg:base-medium text-white">{likes?.length}</p>
+        <p className="small-medium lg:base-medium text-[#877EFF]">{likes?.length > 0 ? likes?.length : ""}</p>
       </div>
 
       <div className="flex gap-2">
