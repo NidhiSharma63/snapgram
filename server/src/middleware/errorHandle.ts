db26b8117bf0957420fb9655bc1a23707e89e4af
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-11-22 09:51:33 +0530

    errorhandle middleware lowercase added

--- server/src/middleware/errorHandle.ts
+++ server/src/middleware/errorHandle.ts
@@ -3,10 +3,10 @@ import { NextFunction, Request, Response } from "express";
 const errorHandle = (error: Error, _req: Request, res: Response, next: NextFunction) => {
   // console.log(error.message, "MSG");
   if (
-    error.message.includes("Missing") ||
-    error.message.includes("exists") ||
-    error.message.includes("invalid") ||
-    error.message.includes("found")
+    error.message.toLocaleLowerCase().includes("missing") ||
+    error.message.toLocaleLowerCase().includes("exists") ||
+    error.message.toLocaleLowerCase().includes("invalid") ||
+    error.message.toLocaleLowerCase().includes("found")
   ) {
     return res.status(400).json({ error: error.message, status: 400 });
   } else {
