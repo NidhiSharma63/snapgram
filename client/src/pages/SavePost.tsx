fb7032b39a654a5361a8508d4831e33d06d6c035
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-11-22 15:12:24 +0530

    userData displayed on save post page

--- client/src/pages/SavePost.tsx
+++ client/src/pages/SavePost.tsx
@@ -1,5 +1,6 @@
 import GridPostList from "@/components/shared/GridPostList";
 import Loader from "@/components/shared/Loader";
+import useAuth from "@/hooks/query/useAuth";
 import usePost from "@/hooks/query/usePost";
 import useSavePost from "@/hooks/query/useSavePost";
 
@@ -8,7 +9,8 @@ function SavePost() {
   const { data: savePosts, isPending: userSavedPostLoading } = useGetAllSavePost();
   const { useGetPostByIds } = usePost();
   const { data: posts, isFetching, isLoading } = useGetPostByIds(savePosts?.[0]?.postId);
-
+  const { useGetAllUser } = useAuth();
+  const { data: usersData } = useGetAllUser();
   if (isLoading || isFetching || userSavedPostLoading || !posts) return <Loader />;
   return (
     <div className="saved-container">
@@ -21,7 +23,11 @@ function SavePost() {
         <Loader />
       ) : (
         <ul className="w-full flex justify-center max-w-5xl gap-9">
-          {posts.length === 0 ? <p className="text-light-4">No available posts</p> : <GridPostList posts={posts} />}
+          {posts.length === 0 ? (
+            <p className="text-light-4">No available posts</p>
+          ) : (
+            <GridPostList posts={posts} usersData={usersData} />
+          )}
         </ul>
       )}
     </div>
