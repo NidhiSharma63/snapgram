024450cd67a42878b011f05a69f1f90f5c44879d
NidhiSharma63 <nidhisharma639593@gmail.com>
2024-01-02 10:51:26 +0530

    loading indicator added while uploading image or delete the image

--- client/src/components/form/PostForm.tsx
+++ client/src/components/form/PostForm.tsx
@@ -1,24 +1,25 @@
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { useToast } from "@/components/ui/use-toast";
-import { postFormSchema } from "@/constant/validation";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+import {useToast} from "@/components/ui/use-toast";
+import {postFormSchema} from "@/constant/validation";
 // import { useAuthContext } from "@/context/AuthContext";
 // import { useCreatePost, useUpdatePost } from "@/lib/react-query/queryAndMutations";
 import FileUploader from "@/components/shared/FileUploader";
 import Loader from "@/components/shared/Loader";
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
-import { IPost } from "@/constant/interfaces";
-import { useUserDetail } from "@/context/userContext";
-import { storage } from "@/firebase/config";
+import {Button} from "@/components/ui/button";
+import {Textarea} from "@/components/ui/textarea";
+import {IPost} from "@/constant/interfaces";
+import {useUserDetail} from "@/context/userContext";
+import {storage} from "@/firebase/config";
 import useLikePost from "@/hooks/query/useLikePost";
 import usePost from "@/hooks/query/usePost";
 import useSavePost from "@/hooks/query/useSavePost";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { deleteObject, getDownloadURL, ref, uploadBytes } from "firebase/storage";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
-import { v4 } from "uuid";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {deleteObject, getDownloadURL, ref, uploadBytes} from "firebase/storage";
+import {useState} from "react";
+import {useForm} from "react-hook-form";
+import {useNavigate} from "react-router-dom";
+import {v4} from "uuid";
 import * as z from "zod";
 
 interface IPostFormProps {
@@ -26,27 +27,28 @@ interface IPostFormProps {
   action: "Create" | "Update";
 }
 
-export default function PostForm({ post, action }: IPostFormProps) {
+export default function PostForm({post, action}: IPostFormProps) {
   //   const { mutateAsync: createPost, isPending: isUploadingPost } = useCreatePost();
 
-  const { useCreatePost, useUpdatePost, useDeletePost } = usePost();
-  const { mutateAsync: updatePost, isPending: isLoadingUpdate } = useUpdatePost();
-  const { mutateAsync: createPost, isPending: isCreatingPost } = useCreatePost();
-  const { mutateAsync: deletePost, isPending: isDeletingPost } = useDeletePost();
+  const {useCreatePost, useUpdatePost, useDeletePost} = usePost();
+  const {mutateAsync: updatePost, isPending: isLoadingUpdate} = useUpdatePost();
+  const {mutateAsync: createPost, isPending: isCreatingPost} = useCreatePost();
+  const {mutateAsync: deletePost, isPending: isDeletingPost} = useDeletePost();
+  const [isPostUploading, setIsPostUploading] = useState(false);
 
   // save post
-  const { useRemoveSave, useGetAllSavePost } = useSavePost();
-  const { mutateAsync: removePostFromSave, isPending: isRemovingPostFromSaveCollection } = useRemoveSave();
-  const { data: savePosts } = useGetAllSavePost();
+  const {useRemoveSave, useGetAllSavePost} = useSavePost();
+  const {mutateAsync: removePostFromSave, isPending: isRemovingPostFromSaveCollection} = useRemoveSave();
+  const {data: savePosts} = useGetAllSavePost();
 
   // like post
-  const { useRemoveLike, useGetAllLike } = useLikePost();
-  const { mutateAsync: removePostFromLike, isPending: isRemovingPostFromLikeCollection } = useRemoveLike();
-  const { data: likePosts } = useGetAllLike();
+  const {useRemoveLike, useGetAllLike} = useLikePost();
+  const {mutateAsync: removePostFromLike, isPending: isRemovingPostFromLikeCollection} = useRemoveLike();
+  const {data: likePosts} = useGetAllLike();
 
   const navigate = useNavigate();
-  const { userDetails } = useUserDetail();
-  const { toast } = useToast();
+  const {userDetails} = useUserDetail();
+  const {toast} = useToast();
 
   const form = useForm<z.infer<typeof postFormSchema>>({
     resolver: zodResolver(postFormSchema),
@@ -65,42 +67,49 @@ export default function PostForm({ post, action }: IPostFormProps) {
   async function onSubmit(values: z.infer<typeof postFormSchema>) {
     values;
     if (post && action === "Update") {
-      const updatedPost = await updatePost({ ...values, file: post.file, _id: post._id });
+      const updatedPost = await updatePost({...values, file: post.file, _id: post._id});
 
       if (!updatedPost) {
-        toast({ title: "Please try again" });
+        toast({title: "Please try again"});
       }
       return navigate(`/posts/${post._id}`);
     }
-    const { file } = values;
-    const imageRef = ref(storage, `/images/${file[0]}-${v4()}`);
-    const snapshot = await uploadBytes(imageRef, file[0]);
-    const url = await getDownloadURL(snapshot.ref);
-    const updatedPayload = { ...values, file: url };
-    const newPost = await createPost({ ...updatedPayload });
-
-    if (!newPost) {
-      toast({ title: "Please try again" });
+    const {file} = values;
+    try {
+      setIsPostUploading(true);
+      const imageRef = ref(storage, `/images/${file[0]}-${v4()}`);
+      const snapshot = await uploadBytes(imageRef, file[0]);
+      const url = await getDownloadURL(snapshot.ref);
+      const updatedPayload = {...values, file: url};
+      await createPost({...updatedPayload});
+    } catch (error) {
+      toast({title: "Please try again"});
+      setIsPostUploading(false);
     }
+
     navigate("/");
+    setIsPostUploading(false);
   }
 
   const handleDeletePost = async () => {
     if (post && userDetails) {
       const storageRef = ref(storage, post.file);
       try {
-        await deletePost({ _id: post._id });
+        setIsPostUploading(true);
+        await deletePost({_id: post._id});
         await deleteObject(storageRef);
         if (savePosts?.[0]?.postId.includes(post._id)) {
-          await removePostFromSave({ postId: post._id, userId: userDetails?._id });
+          await removePostFromSave({postId: post._id, userId: userDetails?._id});
         }
         if (likePosts?.[0]?.postId.includes(post._id)) {
-          await removePostFromLike({ postId: post._id, userId: userDetails?._id });
+          await removePostFromLike({postId: post._id, userId: userDetails?._id});
         }
         navigate("/");
       } catch (error) {
-        toast({ title: "Please try again" });
+        toast({title: "Please try again"});
+        setIsPostUploading(false);
       }
+      setIsPostUploading(false);
     }
   };
 
@@ -110,7 +119,7 @@ export default function PostForm({ post, action }: IPostFormProps) {
         <FormField
           control={form.control}
           name="caption"
-          render={({ field }) => (
+          render={({field}) => (
             <FormItem>
               <FormLabel className="shad-form_label">Caption</FormLabel>
               <FormControl>
@@ -123,7 +132,7 @@ export default function PostForm({ post, action }: IPostFormProps) {
         <FormField
           control={form.control}
           name="file"
-          render={({ field }) => (
+          render={({field}) => (
             <FormItem>
               <FormLabel className="shad-form_label">Add Photos</FormLabel>
               <FormControl>
@@ -136,7 +145,7 @@ export default function PostForm({ post, action }: IPostFormProps) {
         <FormField
           control={form.control}
           name="location"
-          render={({ field }) => (
+          render={({field}) => (
             <FormItem>
               <FormLabel className="shad-form_label">Add Location</FormLabel>
               <FormControl>
@@ -149,7 +158,7 @@ export default function PostForm({ post, action }: IPostFormProps) {
         <FormField
           control={form.control}
           name="tags"
-          render={({ field }) => (
+          render={({field}) => (
             <FormItem>
               <FormLabel className="shad-form_label">Add Tags (separated by comma " , ")</FormLabel>
               <FormControl>
@@ -178,11 +187,15 @@ export default function PostForm({ post, action }: IPostFormProps) {
             <Button
               type="button"
               className="shad-button_dark_4"
-              disabled={isCreatingPost || isLoadingUpdate || isDeletingPost}>
+              disabled={isCreatingPost || isLoadingUpdate || isDeletingPost || isPostUploading}>
               Cancel
             </Button>
           )}
-          {isCreatingPost || isLoadingUpdate || isDeletingPost || isRemovingPostFromSaveCollection ? (
+          {isCreatingPost ||
+          isLoadingUpdate ||
+          isDeletingPost ||
+          isRemovingPostFromSaveCollection ||
+          isPostUploading ? (
             <Button className="shad-button_primary whitespace-nowrap">
               <Loader />
             </Button>
