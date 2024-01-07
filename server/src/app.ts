f25c42f849f9c207e1a055199245682e63ed070c
NidhiSharma63 <nidhisharma639593@gmail.com>
2024-01-07 10:41:59 +0530

    prettier and eslint setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,39 +1,49 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import cors from "cors";
 import * as dotenv from "dotenv";
 import express from "express";
 import errorHandle from "./middleware/errorHandle";
-import router from "./routes/routes";
 import connectDB from "./utils/connectBD";
 dotenv.config();
 
-const cors = require("cors");
-
 const server = express();
 
-// Middleware to parse incoming requests with JSON payloads
-server.use(express.json());
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
 
-// Middleware to parse incoming requests with urlencoded payloads
-server.use(express.urlencoded({ extended: true }));
+// Provide resolver functions for your schema fields
+const resolvers = {
+	Query: {
+		hello: () => "Hello world!",
+	},
+};
 
-// use cors
-server.use(cors());
-server.use("/api/v1", router);
-
-/**
- * handle error
- */
-
-server.use(errorHandle);
+const apolloServer = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
 
 // creating a start function that will connect to database and run the server
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI || "");
-    server.listen(process.env.PORT ?? 5000, () => {
-      console.log("running at port", process.env.PORT ?? 5000);
-    });
-  } catch (error) {
-    console.log("::error::", error);
-  }
+	try {
+		await connectDB(process.env.MONGO_URI || "");
+		await apolloServer.start();
+		server.use(express.json());
+		server.use(express.urlencoded({ extended: true }));
+		server.use(cors());
+
+		/** handle error  */
+		server.use(errorHandle);
+		server.use(expressMiddleware(apolloServer));
+		server.listen(process.env.PORT ?? 5000, () => {
+			console.log("running at port", process.env.PORT ?? 5000);
+		});
+	} catch (error) {
+		console.log("::error::", error);
+	}
 };
 start();
