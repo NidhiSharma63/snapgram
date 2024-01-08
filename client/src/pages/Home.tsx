929ff11874dfc86bcba847ce5e5f466c0dd86718
NidhiSharma63 <nidhisharma639593@gmail.com>
2024-01-08 13:13:52 +0530

    getAll post function query defined

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,33 +3,57 @@ import PostCard from "@/components/shared/PostCard";
 import { IPost, IUser } from "@/constant/interfaces";
 import useAuth from "@/hooks/query/useAuth";
 import usePost from "@/hooks/query/usePost";
+import { gql, useQuery } from "@apollo/client";
 
+const GET_ALL_POSTS = gql`
+	query GetAllPosts {
+		getAllPost {
+			_id
+			caption
+			createdAt
+			file
+			likes
+			location
+			tags
+			userId
+		}
+	}
+`;
 function Home() {
-  const { useGetAllPost } = usePost();
-  const { data: posts, isPending: isPostLoading } = useGetAllPost();
-  const { useGetAllUser } = useAuth();
-  const { data: usersData } = useGetAllUser();
+	const { useGetAllPost } = usePost();
+	const { data, loading } = useQuery(GET_ALL_POSTS);
+	const { data: posts, isPending: isPostLoading } = useGetAllPost();
+	const { useGetAllUser } = useAuth();
+	const { data: usersData } = useGetAllUser();
+	// console.log({ data });
 
-  return (
-    <div className="flex flex-1">
-      <div className="home-container">
-        <div className="home-posts">
-          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
-          {(isPostLoading && !posts) || !usersData ? (
-            <Loader />
-          ) : (
-            <ul className="flex flex-col flex-1 gap-9 w-full">
-              {posts?.map((post: IPost) => {
-                const findCurrentUser = usersData?.find((item: IUser) => item._id === post.userId);
-                return <PostCard key={post._id} post={post} user={findCurrentUser} />;
-              })}
-              {posts?.length === 0 ? <p className="text-center">Create posts to see here!</p> : ""}
-            </ul>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-1">
+			<div className="home-container">
+				<div className="home-posts">
+					<h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+					{loading && !data ? (
+						<Loader />
+					) : (
+						<ul className="flex flex-col flex-1 gap-9 w-full">
+							{data?.getAllPost?.map((post: IPost) => {
+								console.log(post);
+								const findCurrentUser = usersData?.find((item: IUser) => item._id === post.userId);
+								return (
+									<PostCard
+										key={post._id}
+										post={post}
+										user={findCurrentUser}
+									/>
+								);
+							})}
+							{posts?.length === 0 ? <p className="text-center">Create posts to see here!</p> : ""}
+						</ul>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
