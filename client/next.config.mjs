dd7cc32a8f2dafeee1304226ec49643ab685a89d
NidhiSharma63 <nidhisharma639593@gmail.com>
2024-01-27 15:18:40 +0530

    nextjs introduced

---
+++ client/next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+// import withBundleAnalyzer from "@next/bundle-analyzer";
+
+// withBundleAnalyzer({
+//   enabled: process.env.ANALYZE === "true",
+// });
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default nextConfig;
