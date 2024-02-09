babf44c013e0861b71a26d95759d44136eae13db
NidhiSharma63 <nidhisharma639593@gmail.com>
2024-02-09 17:32:36 +0530

    all users displayed

---
+++ client/src/app/all-users/page.tsx
@@ -0,0 +1,32 @@
+import AllUserComponent from "@/src/components/allUsers/allUserComponent";
+import Loader from "@/src/components/shared/Loader";
+import { getAllUser } from "@/src/server/user";
+import { User } from "@/src/types/user";
+
+async function page() {
+  try {
+    const { users: creators } = await getAllUser();
+    return (
+      <div className="common-container">
+        <div className="user-container">
+          <h2 className="h3-bold md:h2-bold text-left w-full">All Users</h2>
+          {!creators ? (
+            <Loader />
+          ) : (
+            <ul className="user-grid">
+              {creators?.map((creator: User) => (
+                <li key={creator?._id} className="flex-1 w-[300px]  ">
+                  <AllUserComponent user={creator} />
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    );
+  } catch (error) {
+    return <div>Something went wrong Error: {error.message}</div>;
+  }
+}
+
+export default page;
---
+++ client/src/components/allUsers/allUserComponent.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Button } from "@/src/components/ui/button";
+import { User } from "@/src/types/user";
+import Image from "next/image";
+import Link from "next/link";
+function AllUserComponent({ user }: { user: User }) {
+  return (
+    <Link href={`/profile/${user._id}`} className="user-card">
+      <Image width={40} height={40} src={user.avatar || "/assets/icons/profile-placeholder.svg"} alt="creator" />
+
+      <div className="flex-center flex-col gap-1">
+        <p className="base-medium dark:text-light-1 text-center line-clamp-1">@{user.username}</p>
+        <p className="small-regular text-light-3 text-center line-clamp-1 flex-wrap">@{user.email}</p>
+      </div>
+
+      <Button type="button" size="sm" className="shad-button_primary px-5">
+        Follow
+      </Button>
+    </Link>
+  );
+}
+
+export default AllUserComponent;
