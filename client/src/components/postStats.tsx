c47a8de8822344952a7820d35986cda5cbc5ad78
NidhiSharma63 <nidhisharma639593@gmail.com>
2024-02-10 13:02:53 +0530

    loading added

--- client/src/components/postStats.tsx
+++ client/src/components/postStats.tsx
@@ -1,15 +1,17 @@
 "use client";
 
+import Loader from "@/src/components/shared/Loader";
 import { useUserPostIdForSaveAndLike } from "@/src/context/userSaveAndLikeContext";
 import { addLike, removeLike } from "@/src/server/like";
 import { User } from "@/src/types/user";
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function PostStats({ likes, postId, activeUser }: { likes: string[]; postId: string; activeUser: User }) {
   const pathname = usePathname();
   const containerStyles = pathname.startsWith("/profile") ? "w-full" : "";
   const { setPostsWhichUserLiked, postsWhichUserLiked } = useUserPostIdForSaveAndLike();
+  const [isPostLikeLoading, setIsPostLikeLoading] = useState(false);
   // const [userLikedPost, setUserLikedPost] = useState(likes.includes(activeUser._id));
 
   useEffect(() => {
@@ -18,22 +20,26 @@ function PostStats({ likes, postId, activeUser }: { likes: string[]; postId: str
     }
   }, []);
   const handleAddLikePost = async () => {
+    setIsPostLikeLoading(true);
     await addLike({ userId: activeUser?._id, postId });
-
     setPostsWhichUserLiked([...postsWhichUserLiked, activeUser._id]);
+    setIsPostLikeLoading(false);
     // setUserLikedPost(true);
   };
 
   const handleRemoveLikePost = async () => {
+    setIsPostLikeLoading(true);
     await removeLike({ userId: activeUser?._id, postId });
 
     setPostsWhichUserLiked(postsWhichUserLiked.filter((item) => item !== activeUser._id));
-    // setUserLikedPost(false);
+    setIsPostLikeLoading(false);
   };
   return (
     <div className={`flex justify-between items-center z-20 ${containerStyles}`}>
       <div className="flex gap-2 mr-5">
-        {postsWhichUserLiked.includes(activeUser._id) ? (
+        {isPostLikeLoading ? (
+          <Loader />
+        ) : postsWhichUserLiked.includes(activeUser._id) ? (
           <img
             alt="like"
             src="/assets/icons/liked.svg"
@@ -52,28 +58,7 @@ function PostStats({ likes, postId, activeUser }: { likes: string[]; postId: str
             className="cursor-pointer"
           />
         )}
-        {/* {userLikePostId === postId &&
-        (fetchingLikeUpdatedData || isAddingItemsToLike || isRemovingPostFromLikeCollection) ? (
-          <Loader />
-        ) : likePostsData && likePostsData?.[0]?.postId.includes(postId) ? (
-          <img
-            alt="like"
-            src="/assets/icons/liked.svg"
-            width={20}
-            height={20}
-            // onClick={handleRemoveLikePost}
-            className="cursor-pointer"
-          />
-        ) : (
-          <img
-            alt="like"
-            src="/assets/icons/like.svg"
-            width={20}
-            height={20}
-            // onClick={handleAddLikePost}
-            className="cursor-pointer"
-          />
-        )} */}
+
         <p className="small-medium lg:base-medium text-[#877EFF]">
           {postsWhichUserLiked?.length > 0 ? likes?.length : ""}
         </p>
