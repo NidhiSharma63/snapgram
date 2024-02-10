ded5303136e30becd28522ea17b23ff17ffef2bb
NidhiSharma63 <nidhisharma639593@gmail.com>
2024-02-10 10:38:28 +0530

    like and save schema defined

---
+++ client/src/schema/likeSchema.ts
@@ -0,0 +1,18 @@
+import mongoose, { Document, Model } from "mongoose";
+
+// Define the IUser interface for the User model
+interface ILikes extends Document {
+  postId: string[];
+  userId: string;
+}
+
+// creating schema
+const postSchema = new mongoose.Schema<ILikes>({
+  userId: String,
+  postId: [String],
+});
+
+// now we need to create the collection
+const Like: Model<ILikes> = mongoose.models.Like || mongoose.model<ILikes>("Like", postSchema);
+
+export default Like;
---
+++ client/src/schema/saveSchema.ts
@@ -0,0 +1,17 @@
+import mongoose, { Document, Model } from "mongoose";
+
+// Define the IUser interface for the User model
+interface ISaves extends Document {
+  userId: string;
+  postId: string[];
+}
+
+// creating schema
+const saveSchema = new mongoose.Schema<ISaves>({
+  postId: [String],
+  userId: String,
+});
+
+// now we need to create the collection
+const Save: Model<ISaves> = mongoose.models.Like || mongoose.model<ISaves>("Save", saveSchema);
+export default Save;
