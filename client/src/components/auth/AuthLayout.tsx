ea7d67bf1aaed56557b40bd05164bcf63a06659d
NidhiSharma02 <nidhi.sharma@zeitview.com>
2024-12-30 18:39:09 +0530

    WIP

--- client/src/components/auth/AuthLayout.tsx
+++ client/src/components/auth/AuthLayout.tsx
@@ -1,25 +1,24 @@
 import { AppConstants } from "@/constant/keys";
 import { getValueFromLS } from "@/lib/utils";
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 
 // Get the token value from local storage if token is present that's means user is autheticated
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  const storedValue = getValueFromLS(AppConstants.USER_DETAILS);
-  const location = useLocation();
+	const storedValue = getValueFromLS(AppConstants.USER_DETAILS);
+	const location = useLocation();
 
-  if (storedValue) {
-    const parsedValue = JSON.parse(storedValue);
-    const isAuthenticated = parsedValue && parsedValue.tokens && parsedValue.tokens[0].token;
-    if (isAuthenticated) {
-      return children;
-    } else {
-      return <Navigate to="/sign-in" replace state={{ from: location }} />;
-    }
-  } else {
-    return <Navigate to="/sign-in" replace state={{ from: location }} />;
-  }
+	if (storedValue) {
+		const parsedValue = JSON.parse(storedValue);
+		const isAuthenticated =
+			parsedValue?.tokens && parsedValue.tokens[0].token;
+		if (isAuthenticated) {
+			return children;
+		} 
+			return <Navigate to="/sign-in" replace state={{ from: location }} />;
+		
+	}
 };
 
 export default AuthLayout;
