741854f879a18fcb95b5750c90ec949adcea24c1
NidhiSharma02 <nidhi.sharma@zeitview.com>
2025-01-01 16:31:50 +0530

    styling fix

---
+++ client/src/components/chat/Message.tsx
@@ -0,0 +1,7 @@
+export default function Message() {
+	return (
+		<div className="common-container w-full border-2 border-gray-300 border-black h-full">
+			Messssages
+		</div>
+	);
+}
--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -1,3 +1,4 @@
+import Message from "@/components/chat/Message";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import useAuth from "@/hooks/query/useAuth";
@@ -17,19 +18,19 @@ export default function Chat() {
 		);
 	}
 	return (
-		<div className="common-container justify-between !p-0 border-2 border-gray-300 border-black rounded-md">
-			<header className="flex items-center justify-start p-4 border-b-2 w-full border-gray-300 border-black gap-4">
+		<div className="common-container !gap-0 !p-0 border-2 border-gray-300 border-black rounded-md !overflow-hidden">
+			<header className="flex items-center justify-start p-4 border-b-2 w-full border-gray-300 border-black gap-4 h-[80px]">
 				<img
 					src={user?.avatar || "/assets/icons/profile-placeholder.svg"}
 					alt={user?.username}
-					className="rounded-full w-14 h-14 object-cover"
+					className="rounded-full w-12 h-12 object-cover"
 				/>
 				<p className="base-medium h3-light dark:text-light text-center line-clamp-1">
 					{user?.username}
 				</p>
 			</header>
-			{"messages"}
-			<div className="flex items-center justify-between p-4  w-full border-gray-300 border-black gap-4">
+			<Message />
+			<div className="flex items-center justify-between p-4 h-[60px] w-full gap-4">
 				<Textarea
 					className="shad-textarea custom-scrollbar textarea-field-chat"
 					placeholder="Type a message here..."
