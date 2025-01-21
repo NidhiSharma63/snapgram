b8727415e17cb099ef9e0d2e68740f5221fff734
NidhiSharma02 <nidhi.sharma@zeitview.com>
2025-01-21 12:48:04 +0530

    middleware added

--- server/src/utils/socket.ts
+++ server/src/utils/socket.ts
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 import type { Server as HTTPServer } from "node:http";
 import { Server as SocketIOServer } from "socket.io";
 import Chat from "../models/messageSchema";
@@ -9,21 +10,38 @@ export const initSocket = (server: HTTPServer) => {
     cors: {
       origin: "*", // Replace with frontend URL in production
       methods: ["GET", "POST"],
+      credentials: true,
+
     },
   });
 
+    // Middleware for authentication
+  io.use(async (socket, next) => {
+    try {
+      const token = socket.handshake.auth.token || socket.handshake.query.token;
+      if (!token) throw new Error("Token is required");
+
+      const secretKey = process.env.SECRET_KEY || "";
+      const decoded = await jwt.verify(token, secretKey);
+      // socket.user = decoded;  // Attach user data for later use
+      next();
+    } catch (error) {
+      console.log(error);
+      next(new Error("Invalid token"));
+    }
+  });
   io.on("connection", (socket) => {
-    console.log(`A user connected: ${socket.id}`);
+    // console.log(`A user connected: ${socket.id}`);
 
     // Handle user joining a room
     socket.on("join-room", async ({ userId, roomId }) => {
       if (!userId || !roomId) {
-        console.error("join-room: Missing userId or roomId");
+        // console.error("join-room: Missing userId or roomId");
         return;
       }
       socket.join(roomId);
       activeUsers.set(socket.id, { userId, roomId });
-      console.log(`[JOIN] ${userId} joined room ${roomId}`);
+      // console.log(`[JOIN] ${userId} joined room ${roomId}`);
     });
 
     // Handle message sending
@@ -49,7 +67,7 @@ export const initSocket = (server: HTTPServer) => {
         // Emit message to everyone in the room
         io.to(roomId).emit("receive-message", newMessage);
 
-        console.log(`[MESSAGE] From ${senderId} to ${receiverId} in room ${roomId}: ${message}`);
+        // console.log(`[MESSAGE] From ${senderId} to ${receiverId} in room ${roomId}: ${message}`);
       } catch (err) {
         console.error("Error saving message:", err);
       }
@@ -58,7 +76,7 @@ export const initSocket = (server: HTTPServer) => {
     // Mark messages as seen
     socket.on("mark-as-seen", async ({ roomId, userId }) => {
       if (!roomId || !userId) {
-        console.error("mark-as-seen: Missing roomId or userId");
+        // console.error("mark-as-seen: Missing roomId or userId");
         return;
       }
 
@@ -78,7 +96,7 @@ export const initSocket = (server: HTTPServer) => {
           );
 
           io.to(roomId).emit("messages-seen", { roomId, userId, timestamp: new Date() });
-          console.log(`[SEEN] Last message in room ${roomId} marked as seen by ${userId}`);
+          // console.log(`[SEEN] Last message in room ${roomId} marked as seen by ${userId}`);
         }
       } catch (err) {
         console.error("Error marking messages as seen:", err);
@@ -96,7 +114,7 @@ export const initSocket = (server: HTTPServer) => {
         // Delete the message from MongoDB
         // await Chat.deleteOne({ _id: messageId });
         io.to(roomId).emit("message-deleted", { messageId ,senderId});
-        console.log(`[DELETE] Message ${messageId} deleted`);
+        // console.log(`[DELETE] Message ${messageId} deleted`);
       } catch (err) {
         console.error("Error deleting message:", err);
       }
@@ -105,28 +123,9 @@ export const initSocket = (server: HTTPServer) => {
     socket.on("disconnect", () => {
       const user = activeUsers.get(socket.id);
       if (user) {
-        console.log(`[DISCONNECT] ${user.userId} left room ${user.roomId}`);
+        // console.log(`[DISCONNECT] ${user.userId} left room ${user.roomId}`);
         activeUsers.delete(socket.id);
       }
-    });
-
-		// Fetch older messages
-		// socket.on("fetch-older-messages", async ({ roomId, lastMessageId }) => {
-    //   if(!lastMessageId){
-    //     const messages = await Chat.find({ roomId })
-		// 		.sort({ timestamp: -1 })
-		// 		.limit(20) // Fetch the previous 20 messages, for example
-		// 		.lean();
-		// 	socket.emit("older-messages", messages);
-    //  return;
-    //   }
-		// 	const messages = await Chat.find({ roomId, _id: { $lt: lastMessageId } })
-		// 		.sort({ timestamp: -1 })
-		// 		.limit(20) // Fetch the previous 20 messages, for example
-		// 		.lean();
-    //   console.log(messages);
-		// 	socket.emit("older-messages", messages);
-		// });
-		
+    });		
   });
 };
