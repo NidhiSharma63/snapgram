8d505e047b689b19f65f5a8fcd0be0d1a56e1a5d
NidhiSharma63 <nidhisharma639593@gmail.com>
2025-06-15 14:15:15 +0530

    logic applied to fetching limited posts

--- client/src/hooks/query/usePost.ts
+++ client/src/hooks/query/usePost.ts
@@ -6,173 +6,203 @@ import {
 	customAxiosRequestForPost,
 } from "@/lib/axiosRequest";
 import { queryClient } from "@/main";
-import { useMutation, useQueries, useQuery } from "@tanstack/react-query";
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQueries,
+  useQuery,
+} from "@tanstack/react-query";
 import type { AxiosError } from "axios";
 
 function usePost() {
-	const queriesToInavlidate = () => {
-		queryClient.invalidateQueries({ queryKey: [QueryKeys.GET_USER_SAVE_POST] });
-		queryClient.invalidateQueries({ queryKey: [QueryKeys.GET_ALL_POSTS] });
-		queryClient.invalidateQueries({ queryKey: [QueryKeys.GET_USER_ALL_POSTS] });
-	};
-	const { toast } = useToast();
-	//create post
+  const queriesToInavlidate = () => {
+    queryClient.invalidateQueries({ queryKey: [QueryKeys.GET_USER_SAVE_POST] });
+    queryClient.invalidateQueries({ queryKey: [QueryKeys.GET_ALL_POSTS] });
+    queryClient.invalidateQueries({ queryKey: [QueryKeys.GET_USER_ALL_POSTS] });
+  };
+  const { toast } = useToast();
+  //create post
 
-	function useCreatePost() {
-		return useMutation({
-			mutationFn: (payload: {
-				caption: string;
-				file: string;
-				location: string;
-				tags: string;
-				userId: string | null;
-				userAvatar: string | null;
-				createdAt: Date;
-			}) => customAxiosRequestForPost("/post", "post", payload),
+  function useCreatePost() {
+    return useMutation({
+      mutationFn: (payload: {
+        caption: string;
+        file: string;
+        location: string;
+        tags: string;
+        userId: string | null;
+        userAvatar: string | null;
+        createdAt: Date;
+      }) => customAxiosRequestForPost("/post", "post", payload),
 
-			onError: (error: AxiosError<ErrorResponse>) => {
-				console.log(error);
+      onError: (error: AxiosError<ErrorResponse>) => {
+        console.log(error);
 
-				if (error.response?.data.status === 400) {
-					toast({
-						title: error.response.data.error,
-					});
-				} else {
-					toast({
-						title: "Something went wrong",
-					});
-				}
-			},
-			onSuccess: () => {
-				queriesToInavlidate();
-			},
-		});
-	}
+        if (error.response?.data.status === 400) {
+          toast({
+            title: error.response.data.error,
+          });
+        } else {
+          toast({
+            title: "Something went wrong",
+          });
+        }
+      },
+      onSuccess: () => {
+        queriesToInavlidate();
+      },
+    });
+  }
 
-	/** update post */
+  /** update post */
 
-	function useUpdatePost() {
-		return useMutation({
-			mutationFn: (payload: {
-				caption: string;
-				file: string;
-				location: string;
-				tags: string;
-				userId: string | null;
-				userAvatar: string | null;
-				createdAt: Date;
-				_id: string;
-			}) => customAxiosRequestForPost("/post", "put", payload),
+  function useUpdatePost() {
+    return useMutation({
+      mutationFn: (payload: {
+        caption: string;
+        file: string;
+        location: string;
+        tags: string;
+        userId: string | null;
+        userAvatar: string | null;
+        createdAt: Date;
+        _id: string;
+      }) => customAxiosRequestForPost("/post", "put", payload),
 
-			onError: (error: AxiosError<ErrorResponse>) => {
-				console.log(error);
+      onError: (error: AxiosError<ErrorResponse>) => {
+        console.log(error);
 
-				if (error.response?.data.status === 400) {
-					toast({
-						title: error.response.data.error,
-					});
-				} else {
-					toast({
-						title: "Something went wrong",
-					});
-				}
-			},
-			onSuccess: () => {
-				queriesToInavlidate();
-			},
-		});
-	}
+        if (error.response?.data.status === 400) {
+          toast({
+            title: error.response.data.error,
+          });
+        } else {
+          toast({
+            title: "Something went wrong",
+          });
+        }
+      },
+      onSuccess: () => {
+        queriesToInavlidate();
+      },
+    });
+  }
 
-	/**
-	 * delete post
-	 */
-	function useDeletePost() {
-		return useMutation({
-			mutationFn: (payload: { _id: string }) =>
-				customAxiosRequestForPost("/post", "delete", payload),
+  /**
+   * delete post
+   */
+  function useDeletePost() {
+    return useMutation({
+      mutationFn: (payload: { _id: string }) =>
+        customAxiosRequestForPost("/post", "delete", payload),
 
-			onError: (error: AxiosError<ErrorResponse>) => {
-				console.log(error);
+      onError: (error: AxiosError<ErrorResponse>) => {
+        console.log(error);
 
-				if (error.response?.data.status === 400) {
-					toast({
-						title: error.response.data.error,
-					});
-				} else {
-					toast({
-						title: "Something went wrong",
-					});
-				}
-			},
-			onSuccess: () => {
-				queriesToInavlidate();
-			},
-		});
-	}
+        if (error.response?.data.status === 400) {
+          toast({
+            title: error.response.data.error,
+          });
+        } else {
+          toast({
+            title: "Something went wrong",
+          });
+        }
+      },
+      onSuccess: () => {
+        queriesToInavlidate();
+      },
+    });
+  }
 
-	function useGetAllPost() {
-		return useQuery({
-			queryKey: [QueryKeys.GET_ALL_POSTS],
-			queryFn: () => customAxiosRequestForGet("/posts", {}),
-		});
-	}
+  // function useGetAllPost() {
+  // 	return useQuery({
+  // 		queryKey: [QueryKeys.GET_ALL_POSTS],
+  // 		queryFn: () => customAxiosRequestForGet("/posts", {}),
+  // 	});
+  // }
 
-	function useGetPostById(id: string) {
-		return useQuery({
-			queryKey: [QueryKeys.GET_POST_BY_ID, id],
-			queryFn: () => customAxiosRequestForGet("/post", {id}),
-		});
-	}
+  interface PostsPage {
+    data: IPost[];
+    hasMore: boolean;
+    currentPage: number;
+    [key: string]: any;
+  }
 
-	function useGetUserAllPost(id: string) {
-		return useQuery({
-			queryKey: [QueryKeys.GET_USER_ALL_POSTS, id],
-			queryFn: () => customAxiosRequestForGet("/user/posts", {id}),
-		});
-	}
+  function useGetAllPost(pageParam?: number) {
+    return useInfiniteQuery<PostsPage, AxiosError<ErrorResponse>>({
+      queryKey: [QueryKeys.GET_ALL_POSTS],
+      queryFn: async () => {
+        return await customAxiosRequestForGet("/posts", {
+          page: pageParam as number,
+          limit: 3,
+        });
+      },
+      initialPageParam: 1,
+      getNextPageParam: (lastPage: PostsPage) => {
+        if (lastPage.hasMore) {
+          return lastPage.currentPage + 1;
+        }
+        return undefined;
+      },
+    });
+  }
+  function useGetPostById(id: string) {
+    return useQuery({
+      queryKey: [QueryKeys.GET_POST_BY_ID, id],
+      queryFn: () => customAxiosRequestForGet("/post", { id }),
+    });
+  }
 
-	function useGetPostByIds(ids: string[]) {
-		const queryResult = useQueries({
-			queries: ids
-				? ids?.map((id) => {
-						return {
-							queryKey: ["post", id],
-							queryFn: () => customAxiosRequestForGet("/post", {id}),
-						};
-					})
-				: [],
-		});
+  function useGetUserAllPost(id: string) {
+    return useQuery({
+      queryKey: [QueryKeys.GET_USER_ALL_POSTS, id],
+      queryFn: () => customAxiosRequestForGet("/user/posts", { id }),
+    });
+  }
 
-		let isLoading=false;
-		let isFetching=false;
-		const allPosts = queryResult?.map((item) => {
-			isLoading = item.isLoading;
-			isFetching = item.isFetching;
-			return item?.data;
-		});
+  function useGetPostByIds(ids: string[]) {
+    const queryResult = useQueries({
+      queries: ids
+        ? ids?.map((id) => {
+            return {
+              queryKey: ["post", id],
+              queryFn: () => customAxiosRequestForGet("/post", { id }),
+            };
+          })
+        : [],
+    });
 
-		// Filter out null values if any post id is saved in saved collection but post
-		// was deleted by user:(second layer)
-		const filteredData = allPosts?.filter(
-			(item: IPost | null) => item !== null,
-		);
+    let isLoading = false;
+    let isFetching = false;
+    const allPosts = queryResult?.map((item) => {
+      isLoading = item.isLoading;
+      isFetching = item.isFetching;
+      return item?.data;
+    });
 
-		return {
-			data: filteredData,
-			isLoading,
-			isFetching,
-		};
-	}
+    // Filter out null values if any post id is saved in saved collection but post
+    // was deleted by user:(second layer)
+    const filteredData = allPosts?.filter(
+      (item: IPost | null) => item !== null
+    );
 
-	return {
-		useCreatePost,
-		useGetPostById,
-		useGetAllPost,
-		useUpdatePost,
-		useDeletePost,
-		useGetPostByIds,
-		useGetUserAllPost,
-	};
+    return {
+      data: filteredData,
+      isLoading,
+      isFetching,
+    };
+  }
+
+  return {
+    useCreatePost,
+    useGetPostById,
+    useGetAllPost,
+    useUpdatePost,
+    useDeletePost,
+    useGetPostByIds,
+    useGetUserAllPost,
+  };
 }
 
 export default usePost;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,27 +3,107 @@ import PostCard from "@/components/shared/PostCard";
 import { IPost, IUser } from "@/constant/interfaces";
 import useAuth from "@/hooks/query/useAuth";
 import usePost from "@/hooks/query/usePost";
+import { useEffect, useState } from "react";
+
+function triggerWhenNidhiJiVisible() {
+  console.log("🚀 'Nidhi ji' is visible now!");
+}
 
 function Home() {
   const { useGetAllPost } = usePost();
-  const { data: posts, isPending: isPostLoading } = useGetAllPost();
+  const [pageParams, setPagesParams] = useState(1);
+  const {
+    data,
+    isPending: isPostLoading,
+    isFetchingNextPage,
+    hasNextPage,
+    fetchNextPage,
+  } = useGetAllPost(pageParams);
   const { useGetAllUser } = useAuth();
   const { data: usersData } = useGetAllUser();
+  const [finalPostsArr, setFinalPostsArr] = useState<IPost[]>([]);
+
+  useEffect(() => {
+    const pagesParams = data?.pageParams;
+    setPagesParams(pagesParams?.[pagesParams?.length - 1] + 1);
+    return data?.pages?.[0]?.data ?? [];
+  }, [data]);
+
+  // get all upcoming post and insert them into a single array
+  useEffect(() => {
+    // console.log(data);
+    const getAllUpcomingPosts =
+      data?.pages?.filter((elem) => {
+        console.log(elem.currentPage, pageParams);
+        return elem.currentPage === pageParams - 1;
+      })?.[0]?.data ?? [];
+
+    if (!getAllUpcomingPosts.length) {
+      return;
+    }
+    setFinalPostsArr((prev) => {
+      return [...prev, ...getAllUpcomingPosts];
+    });
+    console.log(getAllUpcomingPosts, "getAllUpcomingPosts");
+  }, [pageParams, data]);
+
+  console.log("final posts to present on screen", finalPostsArr);
+
+  // console.log({ posts });
+  // check if last post title is in index or not
+  useEffect(() => {
+    // Select all .post-caption elements
+    const allCaptions = document.querySelectorAll(".post-caption");
+
+    // Select the last caption
+    const lastPostTitle = finalPostsArr[finalPostsArr.length - 1]?.caption?.[0];
+
+    // Create an IntersectionObserver
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const el = entry.target as HTMLElement;
+
+          // ✅ Check if this specific caption has lastPostTitle
+          if (
+            entry.isIntersecting &&
+            el.textContent?.trim() === lastPostTitle
+          ) {
+            fetchNextPage();
+          }
+        });
+      },
+      {
+        root: null, // viewport
+        threshold: 0.1, // thoda sa dikhna bhi chalega
+      }
+    );
+
+    allCaptions.forEach((el) => {
+      observer.observe(el);
+    });
+  }, [finalPostsArr, fetchNextPage]);
 
   return (
     <div className="flex flex-1">
       <div className="home-container">
         <div className="home-posts">
           <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
-          {(isPostLoading && !posts) || !usersData ? (
+          {(isPostLoading && !finalPostsArr) || !usersData ? (
             <Loader />
           ) : (
             <ul className="flex flex-col flex-1 gap-9 w-full">
-              {posts?.map((post: IPost) => {
-                const findCurrentUser = usersData?.find((item: IUser) => item._id === post.userId);
-                return <PostCard key={post._id} post={post} user={findCurrentUser} />;
+              {finalPostsArr?.map((post: IPost) => {
+                const findCurrentUser = usersData?.find(
+                  (item: IUser) => item._id === post.userId
+                );
+                return <PostCard post={post} user={findCurrentUser} />;
               })}
-              {posts?.length === 0 ? <p className="text-center">Create posts to see here!</p> : ""}
+              {finalPostsArr?.length === 0 ? (
+                <p className="text-center">Create posts to see here!</p>
+              ) : (
+                ""
+              )}
             </ul>
           )}
         </div>
